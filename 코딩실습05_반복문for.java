c7a3d80f47d3a42e55712589148c8d962890c3a4
unknown <earlycodingx@gmail.com>
2022-01-08 16:54:21 +0900

    first commit

---
+++ 코딩실습05_반복문for.java
@@ -0,0 +1,16 @@
+package elice;
+
+public class 반복문1 {
+
+    public static void main(String[] args) {
+
+        코딩별.출력("식권이 몇 장 필요한가요?");
+        double 장수 = 코딩별.숫자입력();
+
+
+        for (int 출력횟수 = 1; 출력횟수 <= 장수; 출력횟수 = 출력횟수 + 1)
+       {
+            코딩별.출력("[식권] #" + 출력횟수);
+        }
+    }
+}
---
+++ 코딩실습09_클래스만들기/세면도구세트.java
@@ -0,0 +1,9 @@
+package elice;
+
+class 세면도구세트{
+    int 칫솔;
+    int 치약;
+    int 비누;
+    int 샴푸;
+    int 린스;
+}
