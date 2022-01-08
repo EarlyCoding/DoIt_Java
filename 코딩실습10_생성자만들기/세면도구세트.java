e95e4c8c9c84e6f33f3c54b49d04e763b3712cde
unknown <earlycodingx@gmail.com>
2022-01-08 22:58:01 +0900

    completed commit

---
+++ 코딩실습10_생성자만들기/세면도구세트.java
@@ -0,0 +1,17 @@
+package 코딩실습10_생성자만들기;
+
+class 세면도구세트 {
+    int 칫솔;
+    int 치약;
+    int 비누;
+    int 샴푸;
+    int 린스;
+
+    세면도구세트(int A, int B, int C, int D, int E) {
+        칫솔 = A;
+        치약 = B;
+        비누 = C;
+        샴푸 = D;
+        린스 = E;
+    }
+}
---
+++ 코딩실습11_this사용하기/세면도구세트.java
@@ -0,0 +1,17 @@
+package 코딩실습11_this사용하기;
+
+class 세면도구세트 {
+    int 칫솔;
+    int 치약;
+    int 비누;
+    int 샴푸;
+    int 린스;
+
+    세면도구세트(int 칫솔, int 치약, int 비누, int 샴푸, int 린스) {
+        this.칫솔 = 칫솔;
+        this.치약 = 치약;
+        this.비누 = 비누;
+        this.샴푸 = 샴푸;
+        this.린스 = 린스;
+    }
+}
